0fb693ad5300809d9b79a496fbf5527d3bc1a3f9
Sara <saraalamillo93@gmail.com>
2015-02-20 23:54:12 +0100

    Un ListView que visualiza layouts personalizados - Pág. 137

--- app/src/main/java/com/sara/mislugares/MainActivity.java
+++ app/src/main/java/com/sara/mislugares/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Lugares.listaNombres());
+        adaptador = new ArrayAdapter<String>(this, R.layout.elemento_lista, R.id.nombre, Lugares.listaNombres());
         setListAdapter(adaptador);
     }
 
