e4638b9db15aa2ac578d31affd176e4db2ff9795
Sara <saraalamillo93@gmail.com>
2015-02-22 00:28:21 +0100

    ¿Cuándo se llama a los eventos del ciclo de vida en una actividad? - Pág. 154

--- app/src/main/java/com/sara/mislugares/MainActivity.java
+++ app/src/main/java/com/sara/mislugares/MainActivity.java
@@ -31,6 +31,43 @@ public class MainActivity extends ListActivity {
         setContentView(R.layout.activity_main);
         adaptador = new AdaptadorLugares(this);
         setListAdapter(adaptador);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
     @Override
